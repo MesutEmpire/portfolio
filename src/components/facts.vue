78dccb577065c2d99c81ce8dc12b0985f719c168
MesutEmpire <samwainaina01@gmail.com>
2023-01-17 16:32:40 +0300

    Added About ME view

---
+++ src/components/facts.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h2 class="text-3xl font-medium relative inline-block floats-none clear-both before:content-['#'] before:text-primary after:stroke">fun-facts</h2>
+    <div class="flex flex-wrap items-start">
+      <ul class="mt-12 flex gap-4 items-start flex-wrap">
+
+        <li class="p-2 text-gray border border-gray">I like winter more than summer</li>
+
+        <li class="p-2 text-gray border border-gray">I often bike with my friends</li>
+
+        <li class="p-2 text-gray border border-gray">I like pizza and pasta</li>
+
+        <li class="p-2 text-gray border border-gray">I was in Egypt, Poland and Turkey</li>
+
+        <li class="p-2 text-gray border border-gray">I’m still studing in school</li>
+
+        <li class="p-2 text-gray border border-gray">My favorite movie is The Green Mile</li>
+
+      </ul>
+
+    </div>
+
+  </section>
+
+</template>
--- src/components/path.vue
+++ src/components/path.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="self-start block w-full">
-    <h1 class="text-[32px] font-semibold before:content-['/'] before:text-primary">projects</h1>
+    <h1 class="text-[32px] font-semibold before:content-['/'] before:text-primary">{{ finalPath }}</h1>
     <p class="text-gray mt-4">All of my projects</p>
   </div>
 </template>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const {path} = useRoute()
+const removeFirstSlash = path.replace('/','')
+const finalPath = path.replace('/','')
+
+</script>
