68e6bb01fb985c9b1b06c6f00b3fd7009bedf422
MesutEmpire <samwainaina01@gmail.com>
2023-01-18 12:37:22 +0300

    Media Responsiveness

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class=" t-4 md:pt-8 pb-2 bg-background fixed top-0 w-full z-[999]">
+  <div class="t-4 md:pt-8 pb-2 bg-background fixed top-0 w-full z-[999]">
     <input
+        v-model="showNavbar"
       class="before_check  block w-8 m-0 aspect-square md:hidden appearance-none absolute cursor-pointer right-2 top-8 z-[9999] after_check before_checked after_checked"
       type="checkbox"
       aria-label="Menu"
     />
     <div
-      class="fixed hidden lg:flex items-center flex-col justify-center left-4 gap-2 top-0"
+      class="fixed lg:flex items-center flex-col justify-center left-4 gap-2 top-0" :class="{'flex -translate-y-[100%] -translate-x-[50%] z-[9999] left-[50%] mytop text-[24px]':showNavbar,'hidden':!showNavbar}"
     >
-      <span class="w-px h-[33vh] bg-gray block"></span>
-      <div class="grid auto-cols-fr">
+      <span class="w-px h-[33vh] bg-gray" :class="{'hidden':showNavbar,'block':!showNavbar}"></span>
+      <div class="auto-cols-fr" :class="{'flex gap-4':showNavbar,'grid':!showNavbar}">
         <a
           href="https://discord.com/users/914240860101681163"
-          class="w-8 aspect-square"
+          class=" aspect-square"  :class="{'w-12':showNavbar,'w-8':!showNavbar}"
         >
           <img
             src="@/assets/icons/discord.svg"
@@ -20,25 +21,25 @@
             class="w-full aspect-square"
           />
         </a>
-        <a href="https://github.com/EliasDevis" class="w-8 aspect-square">
+        <a href="https://github.com/EliasDevis" class="w-8 aspect-square" :class="{'w-12':showNavbar,'w-8':!showNavbar}">
           <img
             src="@/assets/icons/github.svg"
             alt="github"
             class="w-full aspect-square"
           />
         </a>
-        <a href="mailto:elias@elias-dev.ml" class="w-8 aspect-square">
+        <a href="mailto:elias@elias-dev.ml" class=" aspect-square" :class="{'w-12':showNavbar,'w-8':!showNavbar}">
           <img
             src="@/assets/icons/email.svg"
             alt="email"
-            class="w-full aspect-square"
+            class="w-full aspect-square "
           />
         </a>
       </div>
     </div>
 
-    <div class="my-0 p-4 lg:p-0 mx-auto max-w-5xl relative">
-      <div class="flex w-full justify-between align-center gap-4 relative">
+    <div class=" my-0 p-4 lg:p-0 mx-auto max-w-5xl relative" :class="{'w-full h-[100vh]':showNavbar}" >
+      <div class="flex w-full justify-between align-center gap-4 relative" :class="{'items-start':showNavbar}">
         <a class="flex gap-2" href="/">
           <img
             class="w-4 aspect-square"
@@ -47,7 +48,7 @@
           />
           <span class="font-bold">Sam</span>
         </a>
-        <div class="checked:flex hidden ml-auto md:flex items-start gap-8 text-gray">
+        <div class=" ml-auto md:flex items-start gap-8 text-gray" :class="{'flex absolute flex-col -bottom-8 translate-y-[100%] text-[24px]':showNavbar,'hidden':!showNavbar}">
           <router-link
             to="/"
             class="before:content-['#'] before:text-primary active:text-white active:font-semibold  active:text-white hover:text-white"
@@ -67,7 +68,7 @@
 
           >
         </div>
-        <div class="group hidden md:flex relative text-gray">
+        <div class="group md:flex relative text-gray" :class="{'flex absolute -bottom-4 translate-y-[100%] text-[24px] right-0':showNavbar,'hidden':!showNavbar}">
           <span
             class="hover:text-white cursor-pointer uppercase after:content-['V'] after:inline-block after:font-medium after:ml-1 after:scale-y-[0.5] after:scale-x-100 "
             >en</span
@@ -85,12 +86,32 @@
 </template>
 <script setup lang="ts">
 import {useRoute, useRouter} from "vue-router";
-import {watchEffect} from "vue";
+import {reactive, ref, watchEffect} from "vue";
 const {path} = useRoute()
 const {currentRoute} = useRouter()
 console.log(window.location.pathname)
 console.log(path)
 console.log(currentRoute)
+const showNavbar = ref()
+const errorClass = ref()
+
+
+watchEffect(()=>{
+  console.log(showNavbar.value)
+})
+
+
 
 </script>
-<style scoped></style>
+<style scoped>
+.activeClass {
+  display: flex;
+}
+.notActiveClass{
+  display: none;
+}
+.mytop{
+  top: calc(100% - 16px);
+}
+
+</style>
