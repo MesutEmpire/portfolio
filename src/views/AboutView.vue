5ad3ce77e22c1f84ecd68d97e73f14fdb0f5394f
MesutEmpire <samwainaina01@gmail.com>
2023-01-18 12:46:58 +0300

    Add Class Utilities

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,17 @@
 <template>
   <div
-    class="max-w-5xl p-4 lg:p-0 my-0 mx-auto relative mt-28 flex shrink grow basis-0 flex-col items-center gap-28"
+    class=" max-w-full md:max-w-5xl p-4 lg:p-0 my-0 mx-auto relative mt-28 flex shrink grow basis-0 flex-col items-center gap-20"
   >
-    <h2>About</h2>
+    <Path></Path>
+    <About :class="'mt-0'"></About>
+    <Skills :class="'mb-8 sm:mb-32'"></Skills>
+    <Facts></Facts>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import Path from "@/components/path.vue";
+import About from "@/components/about.vue";
+import Skills from "@/components/skills.vue";
+import Facts from "@/components/facts.vue";
+</script>
