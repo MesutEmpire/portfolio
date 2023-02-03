c7035052fa36b29eb6ca5f2a6faeae56ef1db73a
MesutEmpire <MesutEmpire@users.noreply.github.com>
2023-02-03 16:10:17 +0000

    Deploying to gh-pages from @ MesutEmpire/portfolio@eac152d1fd4b8eddc744fd81a58725f4f9e2a4db 🚀

---
+++ assets/ContactsView-19647e9e.js
@@ -0,0 +1,1 @@
+import{h as e,o as t,c}from"./index-bc271a41.js";const s={},o={class:"max-w-full md:max-w-5xl p-4 lg:p-0 my-0 mx-auto relative mt-28 flex shrink grow basis-0 flex-col items-center gap-28"};function a(r,n){return t(),c("div",o)}const m=e(s,[["render",a]]);export{m as default};
